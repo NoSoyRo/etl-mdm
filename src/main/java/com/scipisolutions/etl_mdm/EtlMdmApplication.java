f41b0d6aa97cb5be15057766bd955ca33d43a24c
Rodrigo <rodri.mo@ciencias.unam.mx>
2025-02-19 18:43:13 -0600

    First commit

---
+++ src/main/java/com/scipisolutions/etl_mdm/EtlMdmApplication.java
@@ -0,0 +1,13 @@
+package com.scipisolutions.etl_mdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlMdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlMdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scipisolutions/etl_mdm/EtlMdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scipisolutions.etl_mdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlMdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
